f63b23addf1f3a9ece86c9dd57f43dabf33e2880
Max Stabel <M4a1x@users.noreply.github.com>
2019-07-05 21:17:41 +0200

    Updated README for LTspice XVII

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-# LTspiceIV
-LTspiceIV unmodified redistribution. Each distribution gets it's own branch. They contain the files of the installation folder of LTspiceIV.
-The master branch contains a script for extracting the installtion folder of the ".exe". Which was a small script for Version 4 and now outdated. I'm keeping it up for personal reference.
+# LTspiceXVII
+LTspiceIV unmodified redistribution. Each distribution gets it's own branch. They contain the files of the installation folder of LTspiceXVII.
+The master branch contains a script for extracting the installtion folder of the ".exe". 
+
+## Contributing
+### Updating
+As this repository is updated by hand and the releases from Linear Technologies are quite frequent, pull requests are welcome.
+
+Steps:
+* Clone the repository: `git clone https://github.com/M4a1x/ltspice.git` (or use SSH if you prefer)
+* Run the script and follow instructions (can be a bit hidden because of wine output) `sh ltspice/extract_install_folder.sh`
+* `cd ltspice`
+* `git checkout -b ${version-number}`
+* `mv ../ltspice-installation/* .`
+* git commit -a -m "Version ${version-number}`
+* git push
 
 Any help and suggestions are welcome. Especially regarding the extraction of the program files without starting the installer, i.e. by using linux tools.
--- extract_install_folder.sh
+++ extract_install_folder.sh
@@ -1,27 +1,40 @@
 #!/bin/sh
 
-#if [ -z "$1" ]
-#  then
-#    echo "No argument supplied. First argument must be LTspiceIV.exe installation file"
-#    exit -1
-#fi
-if [ ! -f LTspiceIV.exe ]; then
-    echo "File not found locally. Trying to fetch from the website.."
-    wget "http://ltspice.linear-tech.com/software/LTspiceIV.exe"
+TMPDIR=/tmp/ltspice
+CURRDIR=$PWD
+OUTDIR=$CURRDIR/ltspice-installation
+WINEDIR=$TMPDIR/env
+DATADIR=$TMPDIR/local
+EXECUTABLE=LTspiceXVII.exe
 
-    if [ $? -ne 0 ]; then
-        echo "Error fetching file."
-        exit -1
-    fi
+echo "Creating tmp directories in $TMPDIR"
+install -m755 -d $TMPDIR $WINEDIR $DATADIR
+cd $TMPDIR
+
+echo "Getting installation file from the website..."
+echo "---------------------------------------------"
+wget "http://ltspice.analog.com/software/LTspiceXVII.exe"
+
+if [ $? -ne 0 ]; then
+    echo "Error fetching file."
+    rm -rf $TMPDIR
+    exit -1
 fi
 
-install -m755 -d tmp tmp/env tmp/local
-export WINEPREFIX="$PWD"/tmp/env
-export XDG_DATA_HOME="$PWD"/tmp/local
-wine LTspiceIV.exe
+echo "Setting environment variables..."
+export WINEPREFIX=$WINEDIR
+export XDG_DATA_HOME=$DATADIR
 
-mv tmp/env/drive_c/Program\ Files\ \(x86\)/LTC/LTspiceIV .
-rm -rf tmp
-rm LTspiceIV.exe
+echo "Starting Installation..."
+echo "Please accept the Terms & Conditions, select 'both x86 & x64' and install to default location"
+echo "---------------------------------------------------------------------------------------------"
+wine LTspiceXVII.exe
+
+echo "Extracting the relevant files..."
+install -m755 -d $OUTDIR
+mv $WINEDIR/drive_c/Program\ Files/LTC/LTspiceXVII/* $OUTDIR
+
+echo "Cleaning up $TMPDIR..."
+rm -rf $TMPDIR
 
 exit 0
